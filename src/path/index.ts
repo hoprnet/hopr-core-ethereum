fd8dfcf58df50cb9e7c7f94e67aee3fd286b52b6
Robert Kiel <robert.kiel@hoprnet.io>
2020-08-21 08:51:41 +0200

    cleanup

--- src/path/index.ts
+++ src/path/index.ts
@@ -19,126 +19,6 @@ class Path {
   constructor(private coreConnector: HoprEthereum) {}
 
   async findPath(start: Public, targetLength: number, filter?: (node: Public) => boolean): Promise<Public[]> {
-    // const cameFrom = new Map<Public, Public>()
-    // const fScore = new Map<Public, number>([[start, 0]])
-
-    // const comparator = (a: Public, b: Public) => {
-    //   if (fScore.get(a) == null || fScore.get(b) == null) {
-    //     throw Error('Comparator: unable to find node')
-    //   }
-
-    //   return fScore.get(b) - fScore.get(a)
-    // }
-
-    // const openList = Heap.heapify([start], comparator)
-
-    // let current: Public = start
-    // let found
-
-    // let add: Public[]
-
-    // while (openList.length > 0) {
-    //   current = openList.pop() as Public
-
-    //   const currentFScore = fScore.get(current)
-
-    //   if (currentFScore == targetLength) {
-    //     return reconstructPath(cameFrom, current, targetLength)
-    //   }
-
-    //   add = []
-
-    //   let newNode: Public
-
-    //   const _newNodes = await this.coreConnector.indexer.get({ partyA: current })
-
-    //   // @
-    //   console.log(`newNodes`, _newNodes.length)
-
-    //   for (let i = 0; i < _newNodes.length; i++) {
-    //     found = false
-
-    //     if (current.eq(_newNodes[i].partyA)) {
-    //       newNode = _newNodes[i].partyB
-    //     } else {
-    //       newNode = _newNodes[i].partyA
-    //     }
-
-    //     const newNodeFScore = fScore.get(newNode)
-
-    //     console.log(`currentFScore`, currentFScore, `newNodeFScore`, newNodeFScore)
-
-    //     if (newNodeFScore == null) {
-    //       cameFrom.set(newNode, current)
-    //       fScore.set(newNode, currentFScore + 1)
-    //       console.log(`setting new fScore`, currentFScore + 1)
-    //       add.push(newNode)
-    //       continue
-    //     }
-
-    //     let tmp = current
-    //     while (true) {
-    //       tmp = cameFrom.get(tmp)
-
-    //       if (tmp == null) {
-    //         break
-    //       }
-
-    //       if (tmp.eq(newNode)) {
-    //         found = true
-    //         break
-    //       }
-    //     }
-
-    //     if (found) {
-    //       console.log(`cycle found`)
-    //       continue
-    //     }
-
-    //     if (newNodeFScore < currentFScore + 1) {
-    //       cameFrom.set(newNode, current)
-    //       fScore.set(newNode, currentFScore + 1)
-    //     }
-
-    //     for (let j = 0; j < openList.length; j++) {
-    //       if (openList.heapArray[j].eq(newNode)) {
-    //         found = true
-    //         console.log(`duplicate found`)
-    //         break
-    //       }
-    //     }
-
-    //     if (!found) {
-    //       add.push(newNode)
-    //     }
-    //   }
-
-    //   openList.addAll(add)
-    //   openList.init()
-    //   console.log(
-    //     `openList`,
-    //     openList.heapArray.map((node: Public) => fScore.get(node))
-    //   )
-
-    //   // console.log(`before adding`, openList.length)
-    //   // console.log(`toAdd`, add.length)
-    //   // if (add.length == 0) {
-    //   //   openList.pop()
-    //   // } else {
-    //   //   openList.replace(add[0])
-
-    //   //   if (add.length > 1) {
-    //   //     openList.addAll(add.slice(1))
-    //   //   }
-    //   // }
-    //   // console.log(`after adding`, openList.length)
-
-    //   add.splice(0, add.length)
-    // }
-
-    // return []
-
-    // @TODO
     const comparator = (a: Entry, b: Entry) => b[2] - a[2]
     type Entry = [Public | undefined, Public, number]
 
@@ -156,7 +36,6 @@ class Path {
     let found
     while (open.length > 0) {
       current = open.pop() as Entry
-      console.log(current[2])
 
       found = false
 
